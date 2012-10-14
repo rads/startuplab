577280d655386177cdda24a0e1d386606f8cbb58
Andrey Kostov <akostov@andrew.cmu.edu>
2012-10-14 15:09:28 -0400

    Installed everything in requirements.txt
    done with the windows virtual env

---
+++ heroku/venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\users\andrey\documents\github\startuplab\heroku\venv\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
