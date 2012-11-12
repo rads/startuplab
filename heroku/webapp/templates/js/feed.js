7ef31e2b9e71636da1bcd03bb69e407fedf7f9e6
Nikolai Mushegian <nmushegi@andrew.cmu.edu>
2012-11-11 20:15:29 -0500

    css/html cleanup

--- heroku/webapp/templates/js/feed.js
+++ heroku/webapp/templates/js/feed.js
@@ -1,32 +1,3 @@
-var colors = ['orange', 'blue', 'grey', 'purple'];
-var BidModel = Backbone.Model.extend({
-    
-});
-
-var BidResults = Backbone.Collection.extend({
-    model: BidModel         
-});
-
-var BidView = Backbone.View.extend({
-    model: BidModel,
-    initialize: function () {
-        this.render();
-    },
-    render: function () {
-        var vars = { 
-            id: this.model.get('id'),
-            title: this.model.get('title'),
-            description: this.model.get('description'),
-            tagstring: this.model.get('tags').join(', '),
-            expire_time_text: this.model.get('expiretime'),
-            color: 'blue',
-            amount: this.model.get('amount'),
-        };
-        var template = _.template($('#bid_template').html(), vars);
-        this.$el.html(template) 
-    },
-});
-
 var BidResultsView = Backbone.View.extend({
 
     initialize: function () {
